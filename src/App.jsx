558abe639f0aa9269e464056f0c6dcf58a1ccb8b
LougerB <glouien@gmail.com>
2023-10-09 14:44:49 +0800

    updated template

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,90 @@
-// import react
-import { useState } from 'react'
-import favico from './assets/react.svg'
-import { Helmet } from 'react-helmet';
-// import styles
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './style.css'
 
 // Layout
-import Header from './project/layout/header/Header';
+import Nav from './Project/Layout/Nav/Nav';
+import Header from './Project/Layout/Header/Header';
+import Footer from './Project/Layout/Footer/Footer';
+// Content
+import Home from './Project/Pages/Home/Home';
+import About from './Project/Pages/About/About';
+import Product from './Project/Pages/Product/Product';
+import Services from './Project/Pages/Services/Services';
+import Contact from './Project/Pages/Contact/Contact';
+import NotFound from './Project/Pages/NotFound/NotFound';
+// css
+import 'bootstrap/dist/css/bootstrap.min.css'
+import favico from './Project/Components/img/favico.png';
+
+// Packages
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import {Helmet} from 'react-helmet';
+import './App.scss'
+import 'sanitize.css';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "/about/",
+    element: <About/>,
+  },
+  {
+    path: "/product/",
+    element: <Product/>,
+  },
+  {
+    path: "/services/",
+    element: <Services/>,
+  },
+  {
+    path: "/contact/",
+    element: <Contact/>,
+  },
+  {
+    path: "*",
+    element: <NotFound/>,
+  },
+]);
+
+const siteName = 'Sample Title';
+const siteDesc = 'Sample Description';
+const siteKeywords = 'Sample Keywords';
+const siteType = 'Sample Type';
 
 function App() {
-	const siteTitle = 'Home';
-	const siteName = 'Site Name';
-	const siteDescription = 'Site Description';
-	const siteKeyWord = 'siteKeyword';
-	const siteOGImage = 'insertUrlHere';
 
   return (
-	<>
-		<Helmet>
-			{/* Meta Tags */}
-			<meta charset="UTF-8"/>
-			<meta name="viewport" content="width=device-width"/>
-			<title>{siteTitle}</title>
-			<link rel="icon" href={favico} />
-			<meta name="description" content={siteDescription}/>
-			<meta name="keywords" content={siteKeyWord}/>
-			<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+       
+    <div className="App">
+    {/* Head */}
+    <Helmet>
+        {/* Meta Tags */}
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width"/>
+        <title>{siteName}</title>
+        <link rel="icon" href={favico} />
+        <meta name="description" content={siteDesc}/>
+        <meta name="keywords" content={siteKeywords}/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 
-			<meta property="og:title" content={siteTitle}/>
-			<meta property="og:description" content={siteDescription}/>
-			<meta property="og:site_name" content={siteName}/>
-			<meta property="og:image" content={siteOGImage}/>
-		</Helmet>
-		
-		<Header />
+        <meta property="og:title" content={siteName}/>
+        <meta property="og:type" content={siteType}/>
+        <meta property="og:description" content={siteDesc}/>
+        <meta property="og:site_name" content={siteName}/>
+    </Helmet>
 
-	</>
+    <Header />
+    <Nav />
+
+    <div className="main">
+        <RouterProvider router={router} />
+    </div>
+
+    <Footer />
+
+</div>
   )
 }
 
